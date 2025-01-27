6239ca1c38ed27a1312c3341f997aa3ca9582c3a
Max C <40443180+makymax@users.noreply.github.com>
2025-01-26 17:07:18 -0800

    Update People.tsx

--- src/pages/People.tsx
+++ src/pages/People.tsx
@@ -30,7 +30,7 @@ export default function People() {
         <div
           className="absolute inset-0 bg-cover bg-center heroStyle"
           style={{
-            backgroundImage: 'url("https://images.unsplash.com/photo-1556761175-b413da4baf72?auto=format&fit=crop&q=80")',
+            backgroundImage: 'url("https://images.unsplash.com/photo-1542744173-8e7e53415bb0?auto=format&fit=crop&q=80")',
            
           }}
         />
